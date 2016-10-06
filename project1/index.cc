1e9f72728066f28b3284e1d9d0afbd8005dbe693
erik <erik@dolins.ky>
2016-10-05 17:08:56 -0700

    added page numbers, switched from global words

--- project1/index.cc
+++ project1/index.cc
@@ -5,17 +5,21 @@
 #include <cctype>       // provides isalpha() and tolower()
 #include <vector>
 #include <algorithm>
+#include <cmath>
+
+#define PAGE_LENGTH 40
 
 struct entry {
     std::string word;
     int count;
+    std::vector<int> pages;
 };
 
-long find(std::string targetWord);
-void insert(std::string);
-void printInOrder(std::ostream & output);
+long find(std::string targetWord, std::vector<entry>& words);
+void insert(std::string, std::vector<entry>& words);
+void printInOrder(std::ostream & output, std::vector<entry>& words);
+void insertUniquePage(int page, std::vector<int>& pages);
 
-std::vector<entry> words;
 
 // The following function should be moved into your skiplist.cc file when you
 // write that.  It implements the random bit generation.
@@ -33,6 +37,29 @@ int randBit(void) {             // return a "random" bit
     bits >>= 1;
     bitsUpperBd >>= 1;
     return b;
+}
+
+/**
+ * Inserts a page number into vector of pages only if the page does not already exist in that vector
+ * @param page integer denoting page number
+ * @param pages vector of page numbers
+ */
+void insertUniquePage(int page, std::vector<int>& pages) {
+    std::vector<int>::iterator it;
+
+    if (pages.size() == size_t(0)) {        // if pages is empty, add first page
+        pages.push_back(page);
+    } else {                                // otherwise, find where page number should belong
+
+        for (it = pages.begin(); it != pages.end(); ++it) {
+            if (*it > page) {           // if value at this index is greater, this is where page number should go
+                break;
+            } else if (*it == page) {   // if this page already exists in list, do nothing
+                return;
+            }
+        }
+        pages.insert(it, page);         // insert page at desired index
+    }
 }
 
 // Remove all characters except letters (A-Z,a-z) from line,
@@ -60,7 +87,7 @@ void print(std::ostream & output, std::string & word) {
  * @param words A vector of entry structs, ordered by the value of their string 'word'
  * @return index of targetWord in words, or -1 if not found
  */
-long find(std::string targetWord) {
+long find(std::string targetWord, std::vector<entry>& words) {
     long bottom = 0;
     long top = (long)words.size() - 1;
     unsigned long middle;
@@ -81,36 +108,52 @@ long find(std::string targetWord) {
     return -1;      // we have not found anything
 }
 
-void insert(std::string newWord) {
+/**
+ *
+ * @param newWord string containing a single word to be inserted
+ * @param words vector of word entries in which to insert newWord
+ * @return index of inserted newWord in words
+ */
+void insert(std::string newWord, std::vector<entry>& words) {
 
     // create new entry
     entry *newEntry = new entry;
     newEntry->word = newWord;
     newEntry->count = 1;
+    std::vector<entry>::iterator it;
 
-    if (words.size() == size_t(0)) {
+    if (words.size() == size_t(0)) {    // if words is empty, add first word
         words.push_back(*newEntry);
-    } else {
+    } else {                            // otherwise, find insertion point
 
-        // Find insertion point and insert new entry
-        std::vector<entry>::iterator it;
         for (it = words.begin(); it != words.end(); ++it) {
-            if (it->word > newWord) {
+            if (it->word > newWord) {   // if word here is alphabetically larger, this is where this word should go
                 break;
             }
         }
-        words.insert(it, *newEntry);
+        words.insert(it, *newEntry);    // insert word here
     }
 }
 
-void printInOrder(std::ostream & output) {
-    std::stringstream info;
-    std::string out;
-    for(std::vector<entry>::iterator it = words.begin(); it != words.end(); it++) {
-        info << it->word << " (" << it->count << ")" << std::endl;
+void printInOrder(std::ostream & output, std::vector<entry>& words) {
+    std::stringstream info; // message buffer
+    std::string out;        // string output
+
+    for( std::vector<entry>::iterator it = words.begin(); it != words.end(); it++) {    // iterate through word list
+        std::string page_separator = "";                // initialize separator for page numbers
+        info << it->word << " (" << it->count << ") ";  // concat word and number of times it was found
+
+        // iterate over page list
+        for (std::vector<int>::iterator page = it->pages.begin(); page != it->pages.end(); page++) {
+
+            info << page_separator << *page;    // add page number
+            page_separator = ",";               // change separator after first iteration
+
+        }
+        info << std::endl;                      // end line after page numbers
     }
-    out = info.str();
-    print(output, out);
+    out = info.str();       // convert buffer to string
+    print(output, out);     // print string
 }
 
 int main(int argc, char *argv[]) {
@@ -134,21 +177,28 @@ int main(int argc, char *argv[]) {
     }
 
     std::string line, word;
+    std::vector<entry> words;
+    int lineNum = 1;    // line tracker
 
-
-    while( !fin.eof() ) {
+    while( !fin.eof() ) {                   // iterate over lines
         getline(fin,line);
-        lowercaseWords(line);
+        lowercaseWords(line);               // convert to lowercase
         std::istringstream iss(line, std::istringstream::in);
-        while( iss >> word ) {
-            long index = find(word);
-            if(index >= 0){
-                words.at((unsigned long)index).count++;
+        while( iss >> word ) {              // iterate over words in line
+            long index = find(word, words); // find word in list of words
+            if(index >= 0){                             // if found
+                words.at((unsigned long)index).count++; // increment count
             } else {
-                insert(word);
+                insert(word, words);                    // if not found, add to list
             }
+
+            // add page number to this word's page list if unique
+            insertUniquePage((int) std::floor(lineNum / PAGE_LENGTH + 1), words.at((unsigned long) find(word, words)).pages);
         }
+        // increment line tracker
+        lineNum++;
     }
 
-    printInOrder(fout);
+    // print result to file
+    printInOrder(fout, words);
 }
